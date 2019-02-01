4da14a1b6b96970fecf63266f7ff6461f610d856
kkaixi <35850858+kkaixi@users.noreply.github.com>
2019-02-01 16:57:14 -0500

    2019-02-01

--- HEV_vs_ICE/AHEC_HEV_vs_ICE_Driver_initialize.py
+++ HEV_vs_ICE/AHEC_HEV_vs_ICE_Driver_initialize.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jul 17 13:35:25 2018
-
+a
 @author: tangk
 """
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from PMG.read_data import initialize
-from PMG.COM.get_props import peakval, get_ipeak, get_tonset, get_i2peak, get_t2peak
-from PMG.COM.easyname import renameISO, rename_list
-#from PMG.COM.arrange import sep_by_peak
+from PMG.COM.get_props import *
+from PMG.COM.easyname import *
 import json
 
 
@@ -57,13 +56,8 @@ channels = ['10CVEHCG0000ACXD',
             '11CLAVLEINTHFOXA',
             '11ILACLE00THFOXA',
             '11ILACRI00THFOXA']
-drop = ['TC13-217',
-        'TC14-012',
-        'TC14-214',
-        'TC12-013',
-        'TC11-007']
 
-table, t, chdata = initialize(directory,channels,cutoff,drop=drop,verbose=False)
+table, t, chdata = initialize(directory,channels,cutoff,verbose=False)
 
 #%% preprocessing
 preprocess_channels = ['11CHSTLEUPTHDSXB','11CHSTRIUPTHDSXB','11CHSTLELOTHDSXB','11CHSTRILOTHDSXB']
@@ -72,99 +66,66 @@ if 'TC13-006' in chdata.index:
     chdata.at['TC13-006','11SEBE0000B6FO0D'] = -chdata.at['TC13-006','11SEBE0000B6FO0D']
 
 #%% feature extraction
-mins, maxs = sep_by_peak(chdata.applymap(peakval))
-tmins, tmaxs = sep_by_peak(chdata.applymap(get_i2peak))
-tmins = get_t2peak(t, tmins)
+def get_all_features(write_csv=False):
+    i_to_t = get_i_to_t(t)
+    feature_funs = {'Min_': [get_min],
+                    'Max_': [get_max],
+                    'Tmin_': [get_argmin,i_to_t],
+                    'Tmax_': [get_argmax,i_to_t]} 
+    features = pd.concat(chdata.chdata.get_features(feature_funs).values(),axis=1,sort=True)
+    if write_csv:
+        features.to_csv(directory + 'features.csv')
+    return features
 
-#%% save features
-mins = mins.rename(lambda name: (name+'_Min'),axis=1).dropna(axis=1,how='all')
-maxs = maxs.rename(lambda name: (name+'_Max'),axis=1).dropna(axis=1,how='all')
-tmins = tmins.rename(lambda name: (name+'_Tmin'),axis=1).dropna(axis=1,how='all')
+features = get_all_features(write_csv=True)
+#%%
+to_JSON = {'project_name': 'HEV_vs_ICE_Driver',
+           'directory'   : directory,
+           'cat'     : {'Series_1_ICE'   : list(table.query('Series_1==1 and Type==\'ICE\'').index),
+                        'Series_1_EV'    : list(table.query('Series_1==1 and Type==\'ICE\'')['Pair']),
+                        'Series_2_ICE_v1': list(table.drop('TC17-025').query('Series_2==1 and Type==\'ICE\'').sort_values('Model').index),
+                        'Series_2_EV_v1' : list(table.drop('TC17-025').query('Series_2==1 and Type==\'EV\'').sort_values('Counterpart').index),
+                        'Series_2_ICE_v2': list(table.drop('TC15-035').query('Series_2==1 and Type==\'ICE\'').sort_values('Model').index),
+                        'Series_2_EV_v2' : list(table.drop('TC15-035').query('Series_2==1 and Type==\'EV\'').sort_values('Counterpart').index)},
+           'data'    : [{'control_ts'    : [directory + 'data\\', 'orig_ts']},
+                        {'features'      : 'features.csv'}],
+           'channels': channels,
+           'cutoff'  : list(cutoff),
+           'test'    : [{'name'       : 'Series_1_wilcox',
+                         'test1'      : 'Series_1_ICE',
+                         'test2'      : 'Series_1_EV',
+                         'paired'     : True,
+                         'testname'   : 'wilcoxon',
+                         'data'       : 'features',
+                         'args'       : 'exact=FALSE,correct=TRUE,conf.level=0.95'},
+                        {'name'       : 'Series_1_t',
+                         'test1'      : 'Series_1_ICE',
+                         'test2'      : 'Series_1_EV',
+                         'paired'     : True,
+                         'testname'   : 't_test',
+                         'data'       : 'features',
+                         'args'       : 'exact=FALSE,correct=TRUE,conf.level=0.95'},
+                        {'name'       : 'Series_2_v1_wilcox',
+                         'test1'      : 'Series_2_v1_ICE',
+                         'test2'      : 'Series_2_v1_EV',
+                         'paired'     : True,
+                         'testname'   : 'wilcoxon'},
+                        {'name'       : 'Series_2_v2_wilcox',
+                         'test1'      : 'Series_2_v2_ICE',
+                         'test2'      : 'Series_2_v2_EV',
+                         'paired'     : True,
+                         'testname'   : 'wilcoxon'},
+                        {'name'       : 'Series_2_v1_t',
+                         'test1'      : 'Series_2_v1_ICE',
+                         'test2'      : 'Series_2_v1_EV',
+                         'paired'     : True,
+                         'testname'   : 't_test'},
+                        {'name'       : 'Series_2_v2_t',
+                         'test1'      : 'Series_2_v2_ICE',
+                         'test2'      : 'Series_2_v2_EV',
+                         'paired'     : True,
+                         'testname'   : 't_test'}], 
+           'test2'  : None}
 
-features = {}
-for dummy in ['TH','H3']:
-    features[dummy] = []
-    for df in [mins,maxs,tmins]:
-        ch = [i for i in df.columns if (i[:2]=='10') or (i[10:12]==dummy)]
-        features[dummy].append(df[ch])
-    features[dummy] = pd.concat(features[dummy],axis=1).rename_axis('TC')
-
-#%% save features and json to file
-if __name__=='__main__':
-    for dummy in ['TH','H3']:
-        features[dummy].to_csv(directory + 'features_' + dummy + '.csv')
-    #%%
-    to_JSON = {'project_name': 'HEV_vs_ICE_Driver',
-               'directory'   : directory,
-               'cat'     : {'control'     : list(table.query('ID11==\'TH\' and Subset==\'CONTROL\'').index)[0::2]+list(table.query('Subset==\'CONTROL\'').index)[1::2],
-                            'control_pair': list(table.query('ID11==\'TH\' and Subset==\'CONTROL\'').index)[1::2]+list(table.query('Subset==\'CONTROL\'').index)[0::2],
-                            'THOR'        : list(table.query('ID11==\'TH\' and Subset==\'HEV vs ICE\'').index[0::2]),
-                            'THOR_pair'   : list(table.query('ID11==\'TH\' and Subset==\'HEV vs ICE\'').index[1::2]),
-                            'H3'          : list(table.query('ID11==\'H3\' and Subset==\'HEV vs ICE\'').index[0::2]),
-                            'H3_pair'     : list(table.query('ID11==\'H3\' and Subset==\'HEV vs ICE\'').index[1::2]),
-                            'double_peak' :   ['TC17-505','TC17-012','TC14-035','TC13-007','TC17-032','TC16-020','TC17-035'],
-                            'no_double_peak': ['TC11-008','TC09-027','TC17-033','TC17-208','TC17-017','TC15-208','TC16-003']},
-               'data'    : [{'control_ts'            : [directory + 'data\\', 'orig_ts']},
-                            {'THOR_injury_metrics'   : ['features_TH.csv','stat']},
-                            {'H3_injury_metrics'     : ['features_H3.csv','stat']},
-                            {'control_injury_metrics': ['features_TH.csv','stat']}],
-               'channels': ['11HEAD0000THACXA',
-                            '11CHST0000THACXC',
-                            '11PELV0000THACXA',
-                            '11FEMRLE00THFOZB',
-                            '11FEMRRI00THFOZB',
-                            '11ACTBLE00THFORB',
-                            '11ACTBRI00THFORB',
-                            '11CHSTRIUPTHDSXB',
-                            '11CHSTRILOTHDSXB',
-                            '11CHSTLEUPTHDSXB',
-                            '11CHSTLELOTHDSXB',
-                            '11SPIN0100THACXC',
-                            '11THSP0100THAVYA',
-                            '11THSP0100THAVZA',
-                            '11SPIN1200THACXC',
-                            '11LUSP0000THFOXA',
-                            '11LUSP0000THFOYA',
-                            '11SEBE0000B3FO0D',
-                            '11SEBE0000B6FO0D'],
-               'cutoff'  : list(cutoff),
-               'test'    : [{'name'        : 'THOR_injury_stats',
-                             'test1'       : 'THOR',
-                             'test2'       : 'THOR_pair',
-                             'paired'      : True,
-                             'testname'    : 'wilcoxon',
-                             'data'        : 'THOR_injury_metrics',
-                             'args'        : 'exact=FALSE,correct=TRUE,conf.level=0.95'},
-                            {'name'       : 'H3_injury_stats',
-                             'test1'      : 'H3',
-                             'test2'      : 'H3_pair',
-                             'paired'     : True,
-                             'testname'   : 'wilcoxon',
-                             'data'       : 'H3_injury_metrics',
-                             'args'       : 'exact=FALSE,correct=TRUE,conf.level=0.95'},
-                            {'name'      : 'control_injury_stats',
-                             'test1'     : 'control',
-                             'test2'     : 'control_pair',
-                             'paired'    : True,
-                             'testname'  : 'wilcoxon',
-                             'data'      : 'control_injury_metrics',
-                             'args'      : 'exact=FALSE,correct=TRUE,conf.level=0.95'},
-                            {'name'     : 'chest_double_peak_comparison',
-                             'test1'    : 'double_peak',
-                             'test2'    : 'no_double_peak',
-                             'paired'   : False,
-                             'testname' : 'wilcoxon',
-                             'data'     : 'control_ts',
-                             'args'     : 'exact=FALSE,correct=TRUE,conf.level=0.95'},
-                            {'name'        : 'control_stats_ts',
-                             'test1'       : 'control',
-                             'test2'       : 'control_pair',
-                             'paired'      : True,
-                             'testname'    : 'wilcoxon',
-                             'data'        : 'control_ts',
-                             'fwe_correct' : False,
-                             'args'        : 'exact=FALSE,correct=TRUE,conf.level=0.95'},], # correction for repeated measures
-               'test2'  : None}
-    
-    with open(directory+'params.json','w') as json_file:
-        json.dump(to_JSON,json_file)
+with open(directory+'params.json','w') as json_file:
+    json.dump(to_JSON,json_file)
